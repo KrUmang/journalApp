782fd5459eda2d7425a2a056f0e10cc542fe2cdb
KrUmang <Umang.k@outlook.in>
2025-05-26 14:01:31 +0530

    new

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -8,6 +8,7 @@ import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -24,13 +25,20 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
+        try {
             User user = userService.findByUserName(userName);
             journalEntry.setDate(LocalDateTime.now());
             JournalEntry saved = journalEntryRepository.save(journalEntry);
             user.getJournalEntries().add(saved);
+            user.setUserName(null);
             userService.saveEntry(user);
-
+        }
+        catch (Exception e){
+            System.out.println(e);
+            throw new RuntimeException("An error occured while saveing the entry");
+        }
 
     }
     public void saveEntry(JournalEntry journalEntry){
