2817d48c9ac11027587404e21d17e94f6bf4348f
KrUmang <Umang.k@outlook.in>
2025-05-29 16:24:36 +0530

    new

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -7,23 +7,62 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Document(collection = "users")
 @Data
-public class User {
+public class User implements UserDetails {
+
     @Id
     private ObjectId id;
 
     @Indexed(unique = true)
     @NonNull
     private String userName;
+
     @NonNull
     private String password;
+
+    private String role = "ROLE_USER";  // default role
+
     @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
 
+    // -- UserDetails interface methods --
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return Collections.singleton(() -> role);
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
