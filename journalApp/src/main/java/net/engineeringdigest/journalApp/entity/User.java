401512bd370bd2aacb521dcaa6a8c721340d7d4d
KrUmang <Umang.k@outlook.in>
2025-05-31 13:11:28 +0530

    new

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -17,7 +17,7 @@ import java.util.List;
 
 @Document(collection = "users")
 @Data
-public class User implements UserDetails {
+public class User  {
 
     @Id
     private ObjectId id;
@@ -29,40 +29,41 @@ public class User implements UserDetails {
     @NonNull
     private String password;
 
-    private String role = "ROLE_USER";  // default role
+    private List<String> roles;  // default role
 
     @DBRef
     private List<JournalEntry> journalEntries = new ArrayList<>();
-
-    // -- UserDetails interface methods --
-
-    @Override
-    public Collection<? extends GrantedAuthority> getAuthorities() {
-        return Collections.singleton(() -> role);
-    }
-
-    @Override
-    public String getUsername() {
-        return userName;
-    }
-
-    @Override
-    public boolean isAccountNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isAccountNonLocked() {
-        return true;
-    }
-
-    @Override
-    public boolean isCredentialsNonExpired() {
-        return true;
-    }
-
-    @Override
-    public boolean isEnabled() {
-        return true;
-    }
 }
+
+//    // -- UserDetails interface methods --
+//
+//    @Override
+//    public Collection<? extends GrantedAuthority> getAuthorities() {
+//        return Collections.singleton(() -> role);
+//    }
+//
+//    @Override
+//    public String getUsername() {
+//        return userName;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return true;
+//    }
+//}
