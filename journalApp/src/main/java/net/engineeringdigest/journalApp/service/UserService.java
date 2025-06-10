4871a1eacfd1b35cce83ce6190e58c5e49d17781
KrUmang <Umang.k@outlook.in>
2025-06-10 14:42:49 +0530

    new

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/UserService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/UserService.java
@@ -1,9 +1,7 @@
 package net.engineeringdigest.journalApp.service;
 
 import lombok.extern.slf4j.Slf4j;
-import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.entity.User;
-import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import net.engineeringdigest.journalApp.repository.UserRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +10,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -27,13 +24,13 @@ public class UserService {
 
     private static final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 
-    public void saveEntry(User user){
+    public void saveNewUser(User user){
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRoles(Arrays.asList("USER"));
-            userRepository.save(user);
+        userRepository.save(user);
 
     }
-    public void saveNewUser(User user){
+    public void saveUser(User user){
 
         userRepository.save(user);
 
